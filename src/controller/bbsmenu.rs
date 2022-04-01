ae738a20a531d49b4f4e714269195eb77af42ce2
haryoiro <mizusecocolte@gmail.com>
2022-04-01 12:41:58 +0900

    normalize menu

--- src/controller/bbsmenu.rs
+++ src/controller/bbsmenu.rs
@@ -1,43 +1,70 @@
 use anyhow::Context;
 use regex::Regex;
 
-use crate::encoder::{is_utf8, sjis_to_utf8};
+use crate::{
+    encoder::{is_utf8, sjis_to_utf8},
+    receiver::Reciever,
+};
 
-fn split_categories(html: &mut String) -> anyhow::Result<Vec<BbsCategories>> {
-    let targetr = Regex::new(r#" TARGET=(.*?)>"#).context("failed to create regex")?;
-    let html = targetr.replace_all(&html, ">".to_string());
+fn normalize_bbsmenu(html: &mut String) -> anyhow::Result<Vec<BbsCategories>> {
+    let html = Regex::new(r#" TARGET=(.*?)>"#)
+        .context("failed to create regex")?
+        .replace_all(&html, ">".to_string());
 
-    // println!("{}", html);
+    let is_utf8 = html.contains("！");
+    let html = if is_utf8 {
+        Regex::new("！")
+            .context("failed to create regex")?
+            .replace_all(&html, "!".to_string())
+    } else {
+        html
+    };
+
     let mut splitted: Vec<&str> = html.split("\n").collect::<Vec<&str>>();
     splitted.reverse();
 
     let mut lines = Vec::new();
-    for l in splitted {
-        if l.starts_with("<A HREF=") {
-            if l.ends_with("</A><br>") {
-                lines.push(&l[8..l.len() - 8]);
-            } else {
-                lines.push(&l[8..l.len() - 4]);
+    for line in splitted {
+        if line.starts_with("<A HREF=") {
+            // リンク
+            if line.ends_with("</A><br>") {
+                // 通常行
+                lines.push(&line[8..line.len() - 8]);
+            } else if line.ends_with("</A>") {
+                // 最終行(<br>無し)
+                lines.push(&line[8..line.len() - 4]);
             };
-        } else if l.starts_with("<BR><BR><B>") {
-            lines.push(&l[11..l.len() - 8]);
+        } else if line.starts_with("<BR><BR><B>") || line.starts_with("<br><br><B>") {
+            // カテゴリ名
+            if line.ends_with("</B><BR>") || line.ends_with("</B><br>") {
+                // 通常
+                lines.push(&line[11..line.len() - 8]);
+            } else {
+                // BBSPINK
+                // スライスだと全角文字が入るとエラーになるので,getを使用する
+                let sliced = line
+                    .get(11..line.len() - 44)
+                    .context("failed to get slice")?;
+                lines.push(sliced);
+            };
+        } else if !line.starts_with("<br><br>") {
+            // コメント入りリンク
+            let ll = line.split("<br>").collect::<Vec<&str>>();
+            for l in ll {
+                if l.starts_with("<A HREF=") && l.ends_with("</A><br>") {
+                    lines.push(&l[8..l.len() - 5]);
+                };
+            }
         }
-    }
+    } // for l in splitted
 
     let mut categories: Vec<BbsCategories> = Vec::new();
     let mut links: Vec<BbsUrl> = Vec::new();
     for l in lines {
-        let s = l.split("/>").collect::<Vec<&str>>();
+        let s = l.split(">").collect::<Vec<&str>>();
         match s.len() {
             1 => {
                 let category = s[0].to_string();
-                if category.contains("<") {
-                    let s = category.split("<").collect::<Vec<&str>>();
-                    let mut title = s[0].to_string();
-                    let url = s[1].to_string();
-                    links.push(BbsUrl { title, url });
-                    continue;
-                }
                 categories.push(BbsCategories {
                     category,
                     list: links,
@@ -45,24 +72,27 @@ fn split_categories(html: &mut String) -> anyhow::Result<Vec<BbsCategories>> {
                 links = Vec::new();
             }
             2 => {
-                let mut title = s[0].to_string();
-                let (en, ..) = encoding_rs::SHIFT_JIS.decode(title.as_bytes());
-                let title = en.to_string();
-                let url = s[1].to_string();
+                let (url, title) = (s[0].to_string(), s[1].to_string());
+                let title = if !is_utf8 {
+                    let (title, ..) = encoding_rs::SHIFT_JIS.decode(title.as_bytes());
+                    title.to_string()
+                } else {
+                    title
+                };
                 links.push(BbsUrl { title, url });
             }
             _ => {
                 return Err(anyhow::anyhow!("failed to parse categories"));
             }
         }
-    }
+    } // for l in lines
 
-    for c in &categories {
-        println!("{}", c.category);
-        for l in &c.list {
-            println!("\t{} : {}", l.title, l.url);
-        }
-    }
+    // for c in &categories {
+    //     println!("{}", c.category);
+    //     for l in &c.list {
+    //         println!("\t{} : {}", l.url, l.title);
+    //     }
+    // }
 
     Ok(categories)
 }
@@ -77,15 +107,14 @@ impl Bbsmenu {
         Self { url }
     }
 
-    pub async fn load(&self) -> anyhow::Result<Vec<BbsUrl>> {
+    pub async fn load(&self) -> anyhow::Result<Vec<BbsCategories>> {
         let url = self.url.clone();
-        let html = Reciever::get(&url).await.context("page error")?.html();
-        let mut text = resp.text().await?;
-        let split = split_categories(&mut text);
-        Ok(Vec::new())
+        let mut html = Reciever::get(&url).await.context("page error")?.html();
+        Ok(normalize_bbsmenu(&mut html).context("failed to parse bbsmenu")?)
     }
 }
 
+#[derive(Debug)]
 pub struct BbsCategories {
     pub category: String,
     pub list: Vec<BbsUrl>,
@@ -93,8 +122,8 @@ pub struct BbsCategories {
 
 #[derive(Debug)]
 pub struct BbsUrl {
-    url: String,
-    title: String,
+    pub url: String,
+    pub title: String,
 }
 
 #[cfg(test)]
@@ -102,11 +131,20 @@ mod tests {
     use super::*;
 
     #[tokio::test]
-    async fn test_get_bbsmenu() {
+    async fn test_get_5ch_bbsmenu() {
         let url = "https://menu.5ch.net/bbsmenu.html";
         let bbsmenu = Bbsmenu::new(url.to_string());
         let result = bbsmenu.load().await;
         println!("{:?}", result);
         assert!(result.is_ok());
     }
+
+    // #[tokio::test]
+    // async fn test_get_2ch_bbsmenu() {
+    //     let url = "https://menu.2ch.sc/bbsmenu.html";
+    //     let bbsmenu = Bbsmenu::new(url.to_string());
+    //     let result = bbsmenu.load().await;
+    //     println!("{:?}", result);
+    //     assert!(result.is_ok());
+    // }
 }
