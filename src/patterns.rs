1dc52a96350476ce42acc5f1e7e608b64d383396
haryoiro <mizusecocolte@gmail.com>
2022-04-15 14:50:48 +0900

    ✨ feat(cli/main.rs): boardviewの大枠
    boardviewの大枠がとりあえず完成
    メッセージが見にくいのでいい感じにフォーマットする必要あり

--- src/patterns.rs
+++ src/patterns.rs
@@ -13,7 +13,9 @@ lazy_static! {
     /// | 3 | date     | 投稿日時    |
     /// | 4 | id       | ユーザID    |
     /// | 5 | message  | 本文        |
-    static ref replies_re:Regex = Regex::new(r###"<div class="meta"><span class="number">(?P<reply_id>\d+)</span><span class="name"><b>(?P<name>.*?)</b></span><span class="date">(?P<date>.*?)</span><span class="uid">(?P<id>.*?)</span></div><div class="message"><span class="escaped">(?P<message>.*?)</span></div>"###).unwrap();
+    pub static ref REPLIES_RE: Regex = Regex::new(r###"<div class="meta"><span class="number">(?P<reply_id>\d+)</span><span class="name"><b>(?P<name>.*?)</b></span><span class="date">(?P<date>.*?)</span><span class="uid">(?P<id>.*?)</span></div><div class="message"><span class="escaped">(?P<message>.*?)</span></div>"###).unwrap();
+
+    pub static ref REPLIES_SC_RE: Regex = Regex::new(r###"<dt>(?P<reply_id>\d+) ：<font.*><b>(?P<name>.*)<[/]b><[/]font>：(?P<date>.*) (?P<id>.*)<dd>(?P<message>.*)"###).unwrap();
 
     /// スレッド一覧ページの文字列をパースする。
     ///
@@ -25,22 +27,18 @@ lazy_static! {
     /// | 2 | index | スレッドインデックス |
     /// | 3 | title | タイトル             |
     /// | 4 | count | レス数               |
-    static ref thread_list_re: Regex = Regex::new(
+    static ref THREAD_LIST_RE: Regex = Regex::new(
             r###"<a href="(?P<id>.*?)/l50">(?P<index>\d+): (?P<title>.*?) \((?P<count>\d+)\)</a>"###
         )
         .expect("failed to create regex");
 
-    static ref stopdone_re: Regex = Regex::new(r###"■ このスレッドは過去ログ倉庫に格納されています"###)
+    pub static ref STOPDONE_RE: Regex = Regex::new(r###"■ このスレッドは過去ログ倉庫に格納されています"###)
             .expect("failed to create regex");
-}
-pub fn parse_replies(before: &str) -> regex::CaptureMatches {
-    replies_re.captures_iter(&before)
+
+    pub static ref ROADING_RE: Regex = Regex::new(r###"読み込み中。。。"###)
+            .expect("failed to create regex");
 }
 
 pub fn parse_thread_list(before: &str) -> regex::CaptureMatches {
-    thread_list_re.captures_iter(&before)
-}
-
-pub fn is_stopdone(before: &str) -> bool {
-    stopdone_re.is_match(&before)
+    THREAD_LIST_RE.captures_iter(&before)
 }
