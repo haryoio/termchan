f4806344427913661ca1bf50bf3da1daf81453e5
haryoiro <mizusecocolte@gmail.com>
2022-04-20 23:47:54 +0900

    🦄 refactor(ui): 冗長なコードに統一感をもたせた
    draw_xxxxx を細かく分割し、コードが持つ責任を小さく

--- cli/src/ui.rs
+++ cli/src/ui.rs
@@ -8,7 +8,7 @@ use std::{
 
 use crate::state::State;
 use tui::{
-    backend::CrosstermBackend,
+    backend::{Backend, CrosstermBackend},
     layout::{Constraint, Direction, Layout, Rect},
     style::{Color, Modifier, Style},
     text::{Span, Spans, Text},
@@ -26,27 +26,10 @@ pub fn draw(frame: &mut Frame<CrosstermBackend<impl Write>>, state: &mut State, 
 
     match current_tab {
         TabItem::Bbsmenu => {
-            let board_chunks = Layout::default()
-                .direction(Direction::Horizontal)
-                .constraints([Constraint::Percentage(20), Constraint::Percentage(80)].as_ref())
-                .split(chunks[0]);
-            let (left, right) = draw_bbsmenu(&mut state.clone());
-            let category_state = &state.clone().category.state;
-            frame.render_stateful_widget(left, board_chunks[0], &mut category_state.to_owned());
-            let board_state = &state.clone().boards.state;
-            frame.render_stateful_widget(right, board_chunks[1], &mut board_state.to_owned());
+            draw_bbsmenu(frame, state, chunks[0]);
         }
         TabItem::Board => {
-            let board_chunk = Layout::default()
-                .direction(Direction::Horizontal)
-                .constraints([Constraint::Percentage(40), Constraint::Percentage(60)].as_ref())
-                .split(chunks[0]);
-
-            let (left, right) = draw_board(&mut state.clone());
-            let thread_list_state = &state.clone().threads.state;
-            frame.render_stateful_widget(left, board_chunk[0], &mut thread_list_state.to_owned());
-            let reply_list_state = &state.clone().thread.state;
-            frame.render_stateful_widget(right, board_chunk[1], &mut reply_list_state.to_owned());
+            draw_board_view(frame, state, chunks[1]);
         }
         TabItem::Settings => todo!(),
     }
@@ -67,19 +50,39 @@ pub fn draw(frame: &mut Frame<CrosstermBackend<impl Write>>, state: &mut State, 
     };
 }
 
-fn draw_bbsmenu<'a>(app: &mut State) -> (List<'a>, List<'a>) {
+fn draw_bbsmenu<'a, B: Backend>(frame: &mut Frame<B>, state: &mut State, chunk: Rect) {
+    let chunks = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([Constraint::Percentage(20), Constraint::Percentage(80)].as_ref())
+        .split(chunk);
+    draw_category_list(frame, state, chunks[0]);
+    draw_board_list(frame, state, chunks[1]);
+}
+
+fn draw_board_view<'a, B: Backend>(frame: &mut Frame<B>, state: &mut State, chunk: Rect) {
+    let chunks = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([Constraint::Percentage(40), Constraint::Percentage(60)].as_ref())
+        .split(chunk);
+    draw_thread_list(frame, state, chunks[0]);
+    draw_thread(frame, state, chunks[1])
+}
+
+fn draw_category_list<'a, B: Backend>(frame: &mut Frame<B>, state: &mut State, chunk: Rect) {
     // カテゴリリスト用のブロックを作成
     let category_list_block = Block::default()
         .borders(Borders::all())
-        .style(Style::default().fg(if app.focus_pane.get() == Pane::Left {
-            Color::White
-        } else {
-            Color::Black
-        }))
+        .style(
+            Style::default().fg(if state.focus_pane.get() == Pane::Left {
+                Color::White
+            } else {
+                Color::Black
+            }),
+        )
         .title("BoardCategory")
         .border_type(BorderType::Plain);
 
-    let category_items: Vec<ListItem> = app
+    let category_items: Vec<ListItem> = state
         .category
         .items
         .iter()
@@ -100,18 +103,25 @@ fn draw_bbsmenu<'a>(app: &mut State) -> (List<'a>, List<'a>) {
                 .add_modifier(Modifier::BOLD),
         );
 
+    let category_state = &state.clone().category.state;
+    frame.render_stateful_widget(category_list, chunk, &mut category_state.to_owned());
+}
+
+fn draw_board_list<'a, B: Backend>(frame: &mut Frame<B>, state: &mut State, chunk: Rect) {
     // 板リスト用のブロックを作成
     let board_block = Block::default()
         .borders(Borders::all())
-        .style(Style::default().fg(if app.focus_pane.get() == Pane::Right {
-            Color::White
-        } else {
-            Color::Black
-        }))
+        .style(
+            Style::default().fg(if state.focus_pane.get() == Pane::Right {
+                Color::White
+            } else {
+                Color::Black
+            }),
+        )
         .title("BoardList")
         .border_type(BorderType::Plain);
 
-    let board_items: Vec<ListItem> = app
+    let board_items: Vec<ListItem> = state
         .boards
         .to_vec()
         .iter()
@@ -130,23 +140,26 @@ fn draw_bbsmenu<'a>(app: &mut State) -> (List<'a>, List<'a>) {
             .add_modifier(Modifier::BOLD),
     );
 
-    (category_list, board_list)
+    let board_state = &state.clone().boards.state;
+    frame.render_stateful_widget(board_list, chunk, &mut board_state.to_owned());
 }
 
-fn draw_board<'a>(app: &mut State) -> (List<'a>, List<'a>) {
+fn draw_thread_list<'a, B: Backend>(frame: &mut Frame<B>, state: &mut State, chunk: Rect) {
     // スレッドリスト用のブロックを作成
     let thread_list_block = Block::default()
         .borders(Borders::all())
-        .style(Style::default().fg(if app.focus_pane.get() == Pane::Left {
-            Color::White
-        } else {
-            Color::Black
-        }))
-        .title(app.current_board().title.clone())
+        .style(
+            Style::default().fg(if state.focus_pane.get() == Pane::Left {
+                Color::White
+            } else {
+                Color::Black
+            }),
+        )
+        .title(state.current_board().title.clone())
         .border_type(BorderType::Plain);
 
     // stateのスレッド一覧をListItemへ変換
-    let thread_items: Vec<ListItem> = app
+    let thread_items: Vec<ListItem> = state
         .threads
         .items
         .iter()
@@ -168,20 +181,26 @@ fn draw_board<'a>(app: &mut State) -> (List<'a>, List<'a>) {
                 .add_modifier(Modifier::BOLD),
         );
 
+    let thread_list_state = &state.clone().threads.state;
+    frame.render_stateful_widget(thread_list, chunk, &mut thread_list_state.to_owned());
+}
+
+fn draw_thread<'a, B: Backend>(frame: &mut Frame<B>, state: &mut State, chunk: Rect) {
     // リプライ用のブロックを作成
     let reply_list_block = Block::default()
         .borders(Borders::all())
-        .style(Style::default().fg(if app.focus_pane.get() == Pane::Right {
-            Color::White
-        } else {
-            Color::Black
-        }))
+        .style(
+            Style::default().fg(if state.focus_pane.get() == Pane::Right {
+                Color::White
+            } else {
+                Color::Black
+            }),
+        )
         .title("Thread")
         .border_type(BorderType::Plain);
 
     // stateからリプライ一覧を取得、ListItemへ変換
-    // TODO: messageからURLなどを抜き出しリンク化
-    let reply_items: Vec<ListItem> = app
+    let reply_items: Vec<ListItem> = state
         .thread
         .to_vec()
         .iter()
@@ -212,7 +231,6 @@ fn draw_board<'a>(app: &mut State) -> (List<'a>, List<'a>) {
         })
         .collect();
 
-    // TODO: Listを作成
     let reply_list = List::new(reply_items)
         .block(reply_list_block)
         .highlight_style(
@@ -222,5 +240,6 @@ fn draw_board<'a>(app: &mut State) -> (List<'a>, List<'a>) {
                 .add_modifier(Modifier::BOLD),
         );
 
-    (thread_list, reply_list)
+    let reply_list_state = &state.clone().thread.state;
+    frame.render_stateful_widget(reply_list, chunk, &mut reply_list_state.to_owned());
 }
